cbe9dc8ed548c9add11d9dd2a4bf39dc7fdf2db3
Alan <alanrdz19@hotmail.com>
2023-08-04 13:06:12 -0600

    VerticalMove config phone and computer

--- Assets/SUBWAY SURFERS/SCRIPTS/PLAYER/VerticalMove.cs
+++ Assets/SUBWAY SURFERS/SCRIPTS/PLAYER/VerticalMove.cs
@@ -1,5 +1,4 @@
-    using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(CheckIfGrounded), typeof(Rigidbody))]
@@ -17,6 +16,11 @@ public class VerticalMove : MonoBehaviour
     private Rigidbody rigidBody;
     private Animator anim;
 
+    private Vector2 startInputPosition; // Aqui se guarda en que parte de la pantalla se empezo a tocar
+    private Vector2 endInputPosition; // Aqui se guarda en que parte de la pantalla se despego el dedo
+
+    private Action VerticalMoveController;
+
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
@@ -26,11 +30,48 @@ public class VerticalMove : MonoBehaviour
 
     private void Update()
     {
-        JumpControl();
-        Roll();
+        VerticalMoveController();
     }
 
-    private void JumpControl()
+    private void VerticalMoveWithKeys()// Este metodo va a ser el como se realiza el salto desde computadora
+    {
+        if (JumpInput() && groundCheck.IsGrounded)
+        {
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (RollInput() && groundCheck.IsGrounded)
+        {
+            anim.Play("Roll");
+        }
+        else if (RollInput() && !groundCheck.IsGrounded)
+        {
+            rigidBody.AddForce(Vector3.down * downForce, ForceMode.Impulse);
+            anim.Play("Roll");
+        }      
+
+    }
+
+    private void VerticalMoveWithTouch()
+    {
+
+        if ((TouchInputSwipe() > 0) && groundCheck.IsGrounded)
+        {
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if ((TouchInputSwipe() < 0) && groundCheck.IsGrounded)
+        {
+            anim.Play("Roll");
+        }
+        else if ((TouchInputSwipe() < 0) && !groundCheck.IsGrounded)
+        {
+            rigidBody.AddForce(Vector3.down * downForce, ForceMode.Impulse);
+            anim.Play("Roll");
+        }
+    }
+
+    private void JumpControlWithKeys() // Este metodo va a ser el como se realiza el salto desde computadora
     {
         if (JumpInput() && groundCheck.IsGrounded)
         {
@@ -44,7 +85,7 @@ public class VerticalMove : MonoBehaviour
         {
             anim.Play("Roll");
         }
-        else if(RollInput() && !groundCheck.IsGrounded)
+        else if (RollInput() && !groundCheck.IsGrounded)
         {
             rigidBody.AddForce(Vector3.down * downForce, ForceMode.Impulse);
             anim.Play("Roll");
@@ -66,15 +107,68 @@ public class VerticalMove : MonoBehaviour
         return isBuffActive ? (jumpForce * jumpBoost) : jumpForce;
     }
 
+    private int TouchInputSwipe()
+    {
+        if (InputTouchBegan()) // Si ya empece a tocar el celular entonces
+        {
+            startInputPosition = InputPosition(); // Voy a guardar mi posicion del input inicial
+        }
+
+        if (InputTouchEnded())
+        {
+            endInputPosition = InputPosition();
+
+            if (endInputPosition.y > startInputPosition.y) //  Si la posicion donde termine de tocar es mayor que donde empece a tocar entonces
+            {
+                return 1; // me dice que deslice hacia arriba
+            }
+            else if (endInputPosition.y < startInputPosition.y) // Si la posicion donde termine de tocar es menor que donde empece a tocar entonces
+            {
+                return -1; // me dice que deslice hacia abajo
+            }
+        }
+        return 0;
+    }
+
+    private Vector2 InputPosition() // Nos regresa donde esta el dedo en la pantalla
+    {
+        return Input.GetTouch(0).position;
+    }
+
+    private bool InputTouchEnded() // Cuando se deja de tocar la pantalla
+    {
+        return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended;
+    }
+
+    private bool InputTouchBegan() // Nos regresa si se esta tocando la pantalla
+    {
+        return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+
     public void ActiveJumpBuff(bool value)
     {
         isBuffActive = value;
     }
 
+    public void PhoneConfig()
+    {
+        VerticalMoveController = VerticalMoveWithTouch;
+    }
+
+    /// <summary>
+    /// Cambia los controles del movimiento horizontal para que funcionen por medio
+    /// de teclas
+    /// 
+    /// **Este metodo se manda a llamar en el Eveneto ComputerConfig del GameManager
+    /// </summary>
+    public void ComputerConfig()
+    {
+        VerticalMoveController = VerticalMoveWithKeys;
+    }
 
     private void OnEnable()
     {
-       
+
     }
 
     private void OnDisable()
